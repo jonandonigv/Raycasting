a599d61a0c4e1883bd0ef3e7567395a230932079
jonandonigv <jonandonigv@gmail.com>
2025-09-28 02:37:03 +0200

    wip: Initialize sdl2 and create a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::time::Duration;
+
+use sdl2::{self, event::Event, keyboard::Keycode, pixels::Color};
+
+const MAP_WIDTH: i32 = 24;
+const MAP_HEIGHT: i32 = 24;
+const SCREEN_WIDTH: u32 = 600;
+const SCREEN_HEIGTH: u32 = 400;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("Raycast", SCREEN_WIDTH, SCREEN_HEIGTH)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
